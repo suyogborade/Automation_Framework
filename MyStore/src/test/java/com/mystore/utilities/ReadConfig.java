8bfc7c6605595a91ad55a92da4765d91b22d3a65
suyogborade <suyog142000@gmail.com>
2024-06-04 14:36:41 +0530

    Hybrid Framework Commit

---
+++ MyStore/src/test/java/com/mystore/utilities/ReadConfig.java
@@ -0,0 +1,54 @@
+package com.mystore.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadConfig {
+
+	Properties properties;
+	
+	String path ="/Users/suyogborade/eclipse-workspace/Automation_Framework/MyStore/Configuration/config.properties";
+	
+	public  ReadConfig()
+	{
+		try {
+		properties = new Properties();
+		
+		FileInputStream file = new FileInputStream(path);
+				properties.load(file);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public String getBaseURL()
+	{
+		String value = properties.getProperty("baseurl");
+		
+		if(value!=null)
+		return value;
+		
+		else 
+		throw new RuntimeException("URL is not specified in the Cofig file");
+		
+	
+	}
+	
+	
+	public String getbrowser()
+	{
+		String brow = properties.getProperty("browser");
+		
+		if(brow!=null)
+			return brow;
+		
+		else
+			throw new RuntimeException("browser is not specified in the config file");
+	}
+	
+	
+}
