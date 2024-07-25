889182b497e326d71ab27d095f6a36cc15d539c4
suyogborade <suyog142000@gmail.com>
2024-07-25 16:26:33 +0530

    Framework changes commit

--- src/test/java/com/utilities/ReadConfig.java
+++ src/test/java/com/utilities/ReadConfig.java
@@ -7,48 +7,42 @@ import java.util.Properties;
 public class ReadConfig {
 
 	Properties properties;
-	
-	String path ="/Users/suyogborade/eclipse-workspace/FrameworkAutomation/Automation_Framework/Configuration/config.properties";
-	
-	public  ReadConfig()
-	{
+
+	String path = "/Users/suyogborade/eclipse-workspace/FrameworkAutomation/Automation_Framework/Configuration/config.properties";
+
+	public ReadConfig() {
 		try {
-		properties = new Properties();
-		
-		FileInputStream file = new FileInputStream(path);
-				properties.load(file);
-			
+			properties = new Properties();
+
+			FileInputStream file = new FileInputStream(path);
+			properties.load(file);
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	public String getBaseURL()
-	{
+
+	public String getBaseURL() {
 		String value = properties.getProperty("baseurl");
-		
-		if(value!=null)
-		return value;
-		
-		else 
-		throw new RuntimeException("URL is not specified in the Cofig file");
-		
-	
+
+		if (value != null)
+			return value;
+
+		else
+			throw new RuntimeException("URL is not specified in the Cofig file");
+
 	}
-	
-	
-	public String getbrowser()
-	{
+
+	public String getbrowser() {
 		String brow = properties.getProperty("browser");
-		
-		if(brow!=null)
+
+		if (brow != null)
 			return brow;
-		
+
 		else
 			throw new RuntimeException("browser is not specified in the config file");
 	}
-	
-	
+
 }
